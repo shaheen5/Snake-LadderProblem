b8ceb9e6ab4ae6e599b815ee38540cddff01f132
unknown <miyashaheen1995@gmail.com>
2021-02-15 22:36:17 +0530

    Added: Code to display player start position at 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,8 @@ public class SnakeAndLadder {
 	
 	public static void main(String args[]) {
 		System.out.println("Welcome to Snake and Ladder Simulation Problem!!!");
+		int start=0;
+		int player_position=start;
+		System.out.println("**************Game Starts***********\n"+"Player position is "+player_position);
 	}
 }
