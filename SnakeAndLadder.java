28b4dc46e653c4f630a467a8df2b5d537d1c890a
unknown <miyashaheen1995@gmail.com>
2021-02-15 22:17:05 +0530

    Added: Welcome message

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package Snake_Ladder_Problem;
+
+public class SnakeAndLadder {
+	
+	public static void main(String args[]) {
+		System.out.println("Welcome to Snake and Ladder Simulation Problem!!!");
+	}
+}
